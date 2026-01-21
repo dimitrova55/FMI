3542e8c9ea8c2731d77169ab8e968d9a1d1c55cb
Dilyana Dimitrova <dilyanadimitrova55@gmail.com>
2026-01-22 03:42:02 +0900

    Initial commit

---
+++ fmi/Controllers/AuthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using RouteAttribute = Microsoft.AspNetCore.Components.RouteAttribute;
+
+namespace fmi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IMongoCollection<User> _users;
+        private readonly IConfiguration _configuration;
+
+    }
+}
---
+++ fmi/Program.cs
@@ -0,0 +1,30 @@
+using MongoDB.Driver;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+var mongoConnectionString = builder.Configuration.GetConnectionString("MongoDb");
+
+builder.Services.AddSingleton<IMongoClient>(new MongoClient(mongoConnectionString));
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
